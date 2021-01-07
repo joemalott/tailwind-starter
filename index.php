9c1ccc2dfcaf9c30208a286cd54eb3d990b4a8fd
Joe <joe@malott.net>
2021-01-07 16:58:21 -0600

    index changes

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <?php wp_head(); ?>
-</head>
-<body>
+<?php get_header(); ?>
  
 <! –– Add Tailwind CSS Stuff Here ––>
 
 
-<?php wp_footer(); ?>
-</body>
-</html>
+
+
+
+<! –– Add Tailwind CSS Stuff Here ––>
+
+
+<?php get_footer(); ?>
