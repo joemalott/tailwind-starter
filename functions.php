19533c1c508b2d411478f9e7cda54bf3a2f939e0
Joe <joe@malott.net>
2021-01-11 16:28:31 -0600

    Updated the Functions and Starting CSS

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function tailwind_theme_name_scripts() {
-	wp_enqueue_style( 'bundle', get_template_directory_uri() . '/dist/bundle.css' );
+    // When you're ready to deploy, build, purge, and uncomment this line here to include the bundled CSS
+    // wp_enqueue_style( 'bundle', get_template_directory_uri() . '/dist/bundle.css' );
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     
 }
--- header.php
+++ header.php
@@ -3,6 +3,11 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <!-- Delete and Enque Purged CSS before Deployment -->
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <!-- This is the CDN version of the big TailwindCSS File -->
+
   <?php wp_head(); ?>
 </head>
 <body class="subpixel-antialiased">
