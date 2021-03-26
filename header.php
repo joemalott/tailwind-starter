d2a0c9996348c2fbc10664386c7ac67f5cb2d21d
Joe Malott <joe@malott.net>
2021-03-25 23:45:52 -0500

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
   <!-- Delete and Enque Purged CSS before Deployment -->
-  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <!-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> -->
   <!-- This is the CDN version of the big TailwindCSS File -->
 
   <?php wp_head(); ?>
