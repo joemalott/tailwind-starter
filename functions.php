e0ae95e139a9134e0f282bdba481675aa4d5ef2e
Joe Malott <joe@malott.net>
2021-03-25 23:46:11 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,11 @@
 <?php
 
 function tailwind_theme_name_scripts() {
-    // When you're ready to deploy, build, purge, and uncomment this line here to include the bundled CSS
-    // wp_enqueue_style( 'app', get_template_directory_uri() . '/css/app.css' );
+    
+    wp_enqueue_style( 'app', get_template_directory_uri() . '/css/app.css' );
     wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+    wp_enqueue_script( 'app', get_template_directory_uri() . '/js/app.js', array(), '1.0.0', true );
     
 }
 add_action( 'wp_enqueue_scripts', 'tailwind_theme_name_scripts' );
