a5d775acbe47832545344c3bb42d28690b9dc532
Joe <joe@malott.net>
2021-03-25 23:28:30 -0500

    changes

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function tailwind_theme_name_scripts() {
     // When you're ready to deploy, build, purge, and uncomment this line here to include the bundled CSS
-    // wp_enqueue_style( 'bundle', get_template_directory_uri() . '/dist/bundle.css' );
+    // wp_enqueue_style( 'app', get_template_directory_uri() . '/css/app.css' );
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     
 }
