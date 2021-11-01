169464263350a3656be600fb9afcda7568a393ba
Joe Malott <joe@malott.net>
2021-10-31 20:59:55 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function tailwind_starter_theme_scripts() {
     
-    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/style.css' );
 
     wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', array(), '1.0.0', true );
     
